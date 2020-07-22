62c51b7a3113b9a63725792c6fa2c2300a65fb8e
Ganesh Kumar <ganesh.kumar@corp.ovh.com>
2020-07-22 12:57:36 +0530

    feat: add account-migration module
    BREAKING CHANGE: init `@ovh-ux/manager-account-migration` module.
    
    a module required for account migration
    
    Fixes #MANAGER-5301
    
    Signed-off-by: Ganesh Kumar <ganesh.kumar@corp.ovh.com>

---
+++ packages/manager/modules/account-migration/src/migration.class.js
@@ -0,0 +1,51 @@
+import find from 'lodash/find';
+import get from 'lodash/get';
+
+import { MIGRATION_STATUS, STEP_STATES, STEP_TYPES } from './constants';
+
+export default class {
+  constructor({ from, id, status, steps, to }) {
+    Object.assign(this, { from, id, status, steps, to });
+  }
+
+  hasOrderPending() {
+    const order = find(this.steps, (step) => step.name === STEP_TYPES.ORDERS);
+    return order.status !== STEP_STATES.OK;
+  }
+
+  hasDebtPending() {
+    const debt = find(this.steps, (step) => step.name === STEP_TYPES.DEBT);
+    return debt.status !== STEP_STATES.OK;
+  }
+
+  getPendingDebt() {
+    const debt = find(this.steps, (step) => step.name === STEP_TYPES.DEBT);
+    return get(debt, 'debt.balanceAmount.value') > 0
+      ? get(debt, 'debt.balanceAmount.text')
+      : get(debt, 'debt.ovhAccountAmount.text');
+  }
+
+  hasNicPending() {
+    const nic = find(this.steps, (step) => step.name === STEP_TYPES.NIC);
+    return nic.status !== STEP_STATES.OK;
+  }
+
+  hasContractsPending() {
+    const contracts = find(
+      this.steps,
+      (step) => step.name === STEP_TYPES.CONTRACTS,
+    );
+    return contracts.status !== STEP_STATES.OK;
+  }
+
+  hasOnlyContractsPending() {
+    return (
+      this.hasContractsPending() &&
+      !(this.hasOrderPending() || this.hasDebtPending() || this.hasNicPending())
+    );
+  }
+
+  isMigrationPending() {
+    return this.status === MIGRATION_STATUS.TODO;
+  }
+}
